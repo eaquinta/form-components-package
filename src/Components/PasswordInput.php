6951bcd53557fb222848548e55497fd006451d74
eaquinta <85019831+eaquinta@users.noreply.github.com>
2024-10-11 12:36:19 -0600

    2024-10-11 12:36

--- src/Components/PasswordInput.php
+++ src/Components/PasswordInput.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 use Illuminate\Support\Facades\Log;
 
-class TextInput extends Component
+class PasswordInput extends Component
 {
     public $prefixId;
     public $name;
