4d9e3c874157990ddccf58f8c53aaae2b8d08987
eaquinta <85019831+eaquinta@users.noreply.github.com>
2024-10-11 10:32:15 -0600

    2024-10-11 10:32

--- src/views/components/button-close.php
+++ src/views/components/button-close.php
@@ -1,1 +1,4 @@
-<button type="button" class="btn btn-outline-danger rounded-2" data-bs-dismiss="modal"><i class="{{ $icon }} me-sm-2 "></i><div class="d-none d-sm-inline">{{ __($lablel) }}</div></button>
+<button type="button" class="btn btn-outline-danger rounded-2" data-bs-dismiss="modal">
+    <i class="{{ $icon }} me-sm-2 "></i>
+    <div class="d-none d-sm-inline">{{ __($label) }}</div>
+</button>
