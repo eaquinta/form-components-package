14736526811140310b1e6bfebd1394e1b5c102cd
Estuardo Quintanilla <eaquinta@outlook.es>
2024-10-18 11:20:15 -0600

    2024-10-18 11:20

--- src/views/components/check-box-input.blade.php
+++ src/views/components/check-box-input.blade.php
@@ -1,13 +1,3 @@
-<label for="{{ $name }}" class="fw-500 fs-rem-80">
-    {{ __($label) }}
-    @if($required)
-        <span class="text-danger">*</span>
-    @endif
-</label>
-<input type="text" name="{{ $name }}" id="{{ $prefixId }}{{ $name }}" class="form-control rounded-1 bg-white" placeholder="{{ __($label) }}" {{ $readOnly ? 'disabled readonly' : ''}}>
-@if (!$readOnly)
-<div class="invalid-feedback"></div>
-@endif
 <div class="form-check form-switch">
     <input class="form-check-input" type="checkbox" id="{{ $prefixId }}{{ $name }}" name="{{ $name }}">
     <label class="form-check-label" for="{{ $name }}">
