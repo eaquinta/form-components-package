50a3a6765f874254b84c25e39aba9d71b8582918
Estuardo Quintanilla <eaquinta@outlook.es>
2024-10-18 08:51:36 -0600

    2024-10-18 08:51

--- src/views/components/switch-input.blade.php
+++ src/views/components/switch-input.blade.php
@@ -1,4 +1,4 @@
 <div class="form-check form-switch">
     <input class="form-check-input" type="checkbox" id="{{ $prefixId }}{{ $name }}" name="{{ $name }}">
-    <label class="form-check-label" for="{{ $name }}">{{ __($label) }}</label>
+    <label class="form-check-label fw-500 fs-rem-80" for="{{ $name }}">{{ __($label) }}</label>
 </div>
