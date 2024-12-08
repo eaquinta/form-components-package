a6cccb41658445bf540ee70cd4057ee3751739e0
Estuardo Quintanilla <eaquinta@outlook.es>
2024-12-07 23:31:12 -0600

    2024-12-07 23:31

--- src/views/components/text-display.blade.php
+++ src/views/components/text-display.blade.php
@@ -6,10 +6,10 @@
         @endif
     </label>
 @endif
-<div style="display: grid;">
+<div style="display: grid; width: 100%; overflow: hidden;">
     <div 
     id="{{ $prefixId }}{{ $name }}" 
-    style="{{ $border ? 'border-bottom: 1px solid #ced4da;' : '' }} height: 25px; white-space: nowrap; font-size: 14px;"
+    style="{{ $border ? 'border-bottom: 1px solid #ced4da;' : '' }} height: 25px; font-size: 14px; max-width: 100%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;"
     >{{ $value }}</div>
 </div>
 
