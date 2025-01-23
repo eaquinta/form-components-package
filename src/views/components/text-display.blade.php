504de7ccfdaaebe75c02ccc7c9aadfa6b57f8b53
eaquinta <85019831+eaquinta@users.noreply.github.com>
2025-01-23 10:44:48 -0600

    2025-01-23 10:44

--- src/views/components/text-display.blade.php
+++ src/views/components/text-display.blade.php
@@ -16,13 +16,3 @@
     style="{{ $border ? 'border-bottom: 1px solid #ced4da;' : '' }} height: 25px; font-size: 14px; max-width: 100%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap; padding: 0px 5px;"
     >{{ $value }}</div>
 </div>
-
-{{-- <input 
-    type="text" 
-    name="{{ $name }}" 
-    id="{{ $prefixId }}{{ $name }}" 
-    class="form-control rounded-1 bg-white {{ $label === false ? 'mt-1' : '' }} {{ $class ?? '' }}" 
-    value="{{ $value }}"
-    {{ $placeholder ? 'placeholder="' . __($label) . '"' : ''}} 
-    {{ $readOnly ? 'disabled readonly' : ''}}>
-<div class="invalid-feedback"></div> --}}
