92ed7c0910df7eb9fe030121abe461da03925ad9
eaquinta <85019831+eaquinta@users.noreply.github.com>
2025-01-24 07:03:27 -0600

    2025-01-24 07:03

--- src/views/components/text-display.blade.php
+++ src/views/components/text-display.blade.php
@@ -1,17 +1,45 @@
 <link href="{{ asset('vendor/fcomponents/form-components.css') }}" rel="stylesheet">
-@if($label !== false)    
+@if($inLine)    
+    <div class="d-flex align-items-center">
+        @if($label !== false)
+            <label for="{{ $name }}" class="fw-500 fs-rem-80" style="{{ $labelWidth ? 'width: ' . $labelWidth . ';' : '' }}">
+                {{ __($label) }}
+                @if($required)
+                    <span class="text-danger">*</span>
+                @endif
+            </label>
+        @endif
+        <div id="{{ $prefixId }}{{ $name }}" 
+            style="flex-grow: 1; font-size: 14px; padding: 0px 5px;">
+            {{ $value }}
+        </div>
+    </div>
+@else
+    @if($label !== false)    
     <label for="{{ $name }}" 
         class="fw-500 fs-rem-80"
-        style="{{ $inLine && $labelWidth ? 'display: inline-block; width: ' . $labelWidth . ';' : '' }} {{ $inLine ? 'margin-right: 10px;' : '' }}">
+        style="">
         {{ __($label) }}
         @if($required)
             <span class="text-danger">*</span>
         @endif
     </label>
+    @endif
+    <div style="display: grid; width: 100%; overflow: hidden;">
+        <div 
+            id="{{ $prefixId }}{{ $name }}" 
+            style="{{ $border ? 'border-bottom: 1px solid #ced4da;' : '' }} 
+                height: 25px; 
+                font-size: 14px; 
+                max-width: 100%; 
+                overflow: hidden; 
+                text-overflow: ellipsis; 
+                white-space: nowrap; 
+                padding: 0px 5px;"
+        >
+            {{ $value }}
+        </div>
+    </div>
 @endif
-<div style="display: {{ $inLine ? 'inline-block' : 'grid' }}; width: 100%; overflow: hidden;">
-    <div 
-    id="{{ $prefixId }}{{ $name }}" 
-    style="{{ $border ? 'border-bottom: 1px solid #ced4da;' : '' }} height: 25px; font-size: 14px; max-width: 100%; overflow: hidden; text-overflow: ellipsis; white-space: nowrap; padding: 0px 5px;"
-    >{{ $value }}</div>
-</div>
+
+
