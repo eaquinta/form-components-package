e49a008bffe3d746f7b9b492bb29986e9bef64d8
Estuardo Quintanilla <eaquinta@yahoo.com>
2025-03-01 09:24:29 -0600

    feat: :sparkles: Version 1.0.0

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInit917b8b2e3addf4009125e39ad51ca050
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit917b8b2e3addf4009125e39ad51ca050::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire917b8b2e3addf4009125e39ad51ca050($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire917b8b2e3addf4009125e39ad51ca050($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
