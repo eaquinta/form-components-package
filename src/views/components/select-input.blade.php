8be31e0683756a3c974c60d719db773f9d2d3f1f
eaquinta <85019831+eaquinta@users.noreply.github.com>
2025-03-13 12:57:31 -0600

    2025-03-13 12:57

--- src/views/components/select-input.blade.php
+++ src/views/components/select-input.blade.php
@@ -1,10 +1,11 @@
-<label for="{{ $name }}" class="fw-500 fs-rem-80">
-    {{ __($label) }}
-    @if($required)
-        <span class="text-danger">*</span>
-    @endif
-</label>
-
+@if($label !== false)
+    <label for="{{ $name }}" class="fw-500 fs-rem-80">
+        {{ __($label) }}
+        @if($required)
+            <span class="text-danger">*</span>
+        @endif
+    </label>
+@endif
 <select class="form-select" name="{{ $name }}" id="{{ $prefixId }}{{ $name }}" data-placeholder="{{ __($label) }}">
     @foreach ($optionsList as $key => $optionValue)
         <option value="{{ $key }}" {{ $key == $value ? 'selected' : '' }}>
--- src/views/components/select-multiple-input.blade.php
+++ src/views/components/select-multiple-input.blade.php
@@ -1,9 +1,11 @@
-<label for="{{ $name }}" class="fw-500 fs-rem-80">
-    {{ __($label) }}
-    @if($required)
-        <span class="text-danger">*</span>
-    @endif
-</label>
+@if($label !== false)
+    <label for="{{ $name }}" class="fw-500 fs-rem-80">
+        {{ __($label) }}
+        @if($required)
+            <span class="text-danger">*</span>
+        @endif
+    </label>
+@endif
 <select class="form-select {{ $class }}" name="{{ $name }}[]" id="{{ $prefixId }}{{ $name }}" multiple="multiple" data-placeholder="{{ __($label) }}"></select>
 @if (!$readOnly)
 <div class="invalid-feedback"></div>
