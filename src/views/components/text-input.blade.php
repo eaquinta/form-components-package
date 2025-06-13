c20f17948d2b3530d0fd1bb97200ab4ffb07e119
Estuardo Quintanilla <eaquinta@yahoo.com>
2025-06-12 20:01:06 -0600

    2025-06-12 20:01

--- src/views/components/text-input.blade.php
+++ src/views/components/text-input.blade.php
@@ -1,4 +1,4 @@
-@if($label !== false)    
+@if($label !== false)
     <label for="{{ $name }}" class="fw-500 fs-rem-80 d-flex justify-content-between">
         <div>
             {{ __($label) }}
@@ -8,7 +8,7 @@
         </div>
         @if($required && $requiredDisable)
             <div class="form-check form-switch d-inline ms-2 align-middle pb-0 mb-0" style="transform: scale(0.8); min-height: 19px;">
-                <input class="form-check-input" type="checkbox" id="switch_required_{{ $name }}" data-bs-toggle="tooltip" data-bs-placement="top" title="campo requerido" checked tabindex="-1">
+                <input class="form-check-input" type="checkbox" id="switch_required_{{ $prefixId }}{{ $id ? $id : $name }}" data-bs-toggle="tooltip" data-bs-placement="top" title="campo requerido" checked tabindex="-1">
             </div>
         @endif
     </label>
