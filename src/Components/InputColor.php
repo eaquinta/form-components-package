9b64bdb87b6481b404e8bdd2173450495717cddd
Estuardo Quintanilla <eaquinta@yahoo.com>
2025-07-13 11:28:35 -0600

    2025-07-13 11:28

---
+++ src/Components/InputColor.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Eaquinta\FormComponentsPackage\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Illuminate\Support\Facades\Log;
+
+class InputColor extends Component
+{
+    public $id;
+    public $name;
+    public $prefixId;
+    public $value;
+    public $label;
+    // public $required;
+    // public $readOnly;
+    // public $placeholder;
+    // public $placeholderText;
+    // public $class;
+    // public $disabled;
+    // public $requiredChar;
+    // public $requiredDisable;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        $id = null,
+        $name,
+        $prefixId = '',
+        $value = null, 
+        $label,
+        // $required = false,
+        // $readOnly = false, 
+        // $placeholder = false,
+        // $placeholderText = null,
+        // $class = '', 
+        // $disabled = false, 
+        // $requiredChar = '*',
+        // $requiredDisable = false
+    )
+    {
+        $this->id               = $id;
+        $this->name             = $name;
+        $this->prefixId         = $prefixId;
+        $this->value            = $value;
+        $this->label            = $label;
+        // $this->required         = $required;
+        // $this->readOnly         = $readOnly;
+        // $this->placeholder      = $placeholder;
+        // $this->placeholderText  = $placeholderText;
+        // $this->class            = $class;
+        // $this->disabled         = $disabled;
+        // $this->requiredChar     = $requiredChar;
+        // $this->requiredDisable  = $requiredDisable;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('fcomponents::components.input-color');
+    }
+}
---
+++ src/views/components/input-color.blade.php
@@ -0,0 +1,2 @@
+<label for="{{ $name }}" class="form-label">{{ __($label) }}}</label>
+<input type="color" class="form-control form-control-color" name="{{ $name }}" id="{{ $prefixId }}{{ $id ? $id : $name }}" value="{{ $value }}">
