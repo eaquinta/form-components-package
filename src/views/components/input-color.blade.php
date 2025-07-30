1c5f652f5b3f29ca1070ba66d7d63bdbf5e4dacb
eaquinta <85019831+eaquinta@users.noreply.github.com>
2025-07-30 14:49:10 -0600

    2025-07-30 14:49

--- src/views/components/input-color.blade.php
+++ src/views/components/input-color.blade.php
@@ -1,2 +1,3 @@
-<label for="{{ $name }}" class="form-label">{{ __($label) }}}</label>
+<label for="{{ $name }}" class="form-label">{{ __($label) }}</label>
 <input type="color" class="form-control form-control-color" name="{{ $name }}" id="{{ $prefixId }}{{ $id ? $id : $name }}" value="{{ $value }}">
+<div class="invalid-feedback"></div>
